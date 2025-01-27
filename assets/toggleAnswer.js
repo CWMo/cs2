f2cdfb44c6a4c524a46430ae432076990cef2238
Chau W. Mo <albertmcw@gmail.com>
2025-01-27 22:16:50 +1100

    adjust font size and line spacing

--- assets/toggleAnswer.js
+++ assets/toggleAnswer.js
@@ -1,6 +1,5 @@
 function toggleAns(question) {
     let answer = question.nextElementSibling;
-    console.dir(answer);
     if (answer.matches(".show")) {
         answer.classList.remove("show")
     } else {
